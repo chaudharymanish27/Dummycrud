681120229f88b6350deac62867667017bf205c7a
Manish chaudhary <72908959+chaudharymanish27@users.noreply.github.com>
2022-05-11 16:12:58 +0530

    Update updateUser.js
    minor change in update  file

--- updateUser.js
+++ updateUser.js
@@ -5,13 +5,17 @@ module.exports.update = async (event) => {
     const requestBody = JSON.parse(event.body)
     const scanParams = {
     TableName: process.env.DYNAMODB_CUSTOMER_TABLE,
-    Item: {
-        'primary_key': requestBody.primary_key,
-        'Name': requestBody.updateValue
+    Key : {
+        'primary_key': requestBody.primary_key
       },
+      UpdateExpression: 'set ' + requestBody.updateKey  +' = :value',
+       ExpressionAttributeValues:{
+        ":value": requestBody.updateValue
+    },
+    ReturnValues:"UPDATED_NEW"
   };
   const dynamodb = new AWS.DynamoDB.DocumentClient();
-  const result = await dynamodb.put(scanParams).promise().then((response) => {
+  const result = await dynamodb.update(scanParams).promise().then((response) => {
      return {      
       UpdatedAttributes: response
     }
